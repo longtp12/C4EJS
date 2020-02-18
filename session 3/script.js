d8657161b343a9ead86bc4cec7d676a2d64b781a
longtp <longtp6@gmail.com>
2020-02-18 21:03:40 +0700

    đ

---
+++ session 3/script.js
@@ -0,0 +1,45 @@
+// sử dụng chuỗi
+// let x = 10;
+
+// let y = 4 * (Math.pow(x,2) + 10 * x * Math.sqrt(Math.pow(x,2) + 3*x + 1));
+
+// console.log(y);
+
+
+// let yourName = prompt("Nhap ten");
+
+// alert(`Xin Chao ${yourName}`);
+
+// chuyen 1 chuoi thanh so
+
+// let x = Number("5");
+// let y = Number("4");
+// console.log(x + y);
+
+// chuyen 1 chuoi ve so nguyen 
+
+// x = parseInt("5.6")
+// console.log(x);
+
+// // chuyen so thanh chuoi
+
+// let s = 30 + ``; //gia tri s=30
+
+// let r =parseFloat(prompt("Nhap ban kinh hinh tron"));
+
+// let c = 2*r*Math.PI;
+// let s = Math.PI * r**2;
+// alert(`Chu vi la ${c}`);
+// alert(`Dien tich la ${s}`);
+
+// chu vi va dien tich chu nhat 
+
+let a = parseFloat(prompt("Nhap chieu dai hinh chu nhat"));
+let b = parseFloat(prompt("Nhap chieu rong hinh chu nhat"));
+let P = 2*(a+b);
+let S = a*b;
+
+alert(`Chu vi hinh chu nhat la ${P}`);
+alert(`Dien tich hinh chu nhat la ${S}`);
+
+console.log(typeof a);
