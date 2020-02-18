ece5275fb9818e713d3fa576a5ed2c7abc9a9326
longtp <longtp6@gmail.com>
2020-02-18 19:41:13 +0700

    session 2

---
+++ session 2_var/script.js
@@ -0,0 +1,77 @@
+
+// // alert("Welcome C4EJS"); //thong bao
+
+// // prompt("What your name??"); //thong tin ng dung
+
+
+// let age; //khai bao bien
+// age = 25; //gan bien
+
+// //  25 = age //sai
+
+// let age1 = 25;
+
+// //sử dụng
+// // alert(age);
+// // alert(age1);
+
+// console.log(age);
+// console.log(age1);
+
+
+
+// var name;
+// const Pi = 3.14;
+
+// let a = 5;
+// let b = 10;
+
+// let c = a + b;
+
+// console.log(c);
+// // - * /
+
+// let x = a - b;
+// let y = a * b;
+// let z = a / b;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// let yourname = prompt("Nhap ten");
+// let yourage = prompt("Nhap tuoi");
+
+// let year = prompt("Nhap nam sinh");
+
+// let yourage = 2020 - year;
+// alert(yourage);
+
+
+// Nhap 2 so tinh + - * / tu 2 so 
+
+let a = prompt("Nhap so a");
+let b = prompt("Nhap so b");
+
+alert(a - b);
+alert(a + b);
+alert(a * b);
+alert(a / b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
